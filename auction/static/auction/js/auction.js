9aa27c1d7fe9745d36abc9e155741885306df47e
Sergio2405 <67673799+Sergio2405@users.noreply.github.com>
2022-08-07 20:23:45 -0500

    validations of fields logic of auction

--- auction/static/auction/js/auction.js
+++ auction/static/auction/js/auction.js
@@ -1,85 +1,217 @@
 function HighOfferToBuy() {
+    
+    let holdings = parseFloat(document.getElementById("Total").value);
+
+    let price = parseFloat(document.getElementById("HighBuyLimitPrice").value);
+    let quantity = parseInt(document.getElementById("HighBuyLimitQuantity").value)
+
     let offer = {
         Asset : "High",
         Type : "Limit",
         Action : "Buy",
-        Price : parseFloat(document.getElementById("HighBuyLimitPrice").value),
-        Quantity : parseInt(document.getElementById("HighBuyLimitQuantity").value)
+        Price : price,
+        Quantity : quantity
     }
-    liveSend(offer)
+
+    let amount = price * quantity;
+
+    if (amount > holdings){
+        alert(`
+            \t NO PROCEDE LA OFERTA DE COMPRA \t \n
+            Nota: \t
+            • Ofreces pagar un precio: ${price} por una cantidad: ${quantity} 
+            • lo cual supera tu actual presupuesto de: ${holdings} 
+        `)
+    }else{
+        liveSend(offer)
+    }
 }
 
 function HighOfferToSell() {
+
+    let high_risk_quantity = parseInt(document.getElementById("High").value);
+
+    let price = parseFloat(document.getElementById("HighSellLimitPrice").value);
+    let quantity = parseInt(document.getElementById("HighSellLimitQuantity").value)
+
     let offer = {
         Asset : "High",
         Type : "Limit",
         Action : "Sell",
-        Price : parseFloat(document.getElementById("HighSellLimitPrice").value),
-        Quantity : parseInt(document.getElementById("HighSellLimitQuantity").value)
+        Price : price,
+        Quantity : quantity
     }
-    liveSend(offer)
+  
+    if ((quantity >= high_risk_quantity)){
+        alert(`
+            \t NO PROCEDE LA OFERTA DE VENTA \t \n
+            Nota: \t
+            • Ofreces vender una cantidad: ${quantity} 
+            • la cual supera la cantidad que tienes de este activo: ${high_risk_quantity} 
+        `)
+    }else{
+        liveSend(offer)
+    }
 }
 
 function LowOfferToBuy() {
+
+    let holdings = parseFloat(document.getElementById("Total").value);
+
+    let price = parseFloat(document.getElementById("LowBuyLimitPrice").value);
+    let quantity = parseInt(document.getElementById("LowBuyLimitQuantity").value)
+
     let offer = {
         Asset : "Low",
         Type : "Limit",
         Action : "Buy",
-        Price : parseFloat(document.getElementById("LowBuyLimitPrice").value),
-        Quantity : parseInt(document.getElementById("LowBuyLimitQuantity").value)
+        Price : price,
+        Quantity : quantity
     }
-    liveSend(offer)
+
+    let amount = price * quantity;
+
+    if (amount > holdings){
+        alert(`
+            \t NO PROCEDE LA OFERTA DE COMPRA \t \n
+            Nota: \t
+            • Ofreces pagar un precio: ${price} por una cantidad: ${quantity} 
+            • lo cual supera tu actual presupuesto de: ${holdings} 
+        `)
+    }else{
+        liveSend(offer)
+    }
 }
 
 function LowOfferToSell() {
+
+    let low_risk_quantity = parseInt(document.getElementById("Low").value);
+
+    let price = parseFloat(document.getElementById("LowSellLimitPrice").value);
+    let quantity = parseInt(document.getElementById("LowSellLimitQuantity").value)
+    
     let offer = {
         Asset : "Low",
         Type : "Limit",
         Action : "Sell",
-        Price : parseFloat(document.getElementById("LowSellLimitPrice").value),
-        Quantity : parseInt(document.getElementById("LowSellLimitQuantity").value)
+        Price : price,
+        Quantity : quantity
     }
-    liveSend(offer)
+
+    if ((quantity >= low_risk_quantity)){
+        alert(`
+            \t NO PROCEDE LA OFERTA DE VENTA \t \n
+            Nota: \t
+            • Ofreces vender una cantidad: ${quantity} 
+            • la cual supera la cantidad que tienes de este activo: ${low_risk_quantity} 
+        `)
+    }else{
+        liveSend(offer)
+    }
 }
 
 function HighSell() {
+
+    let high_risk_quantity = parseInt(document.getElementById("High").value);
+
+    let quantity = parseInt(document.getElementById("HighSellMarket").value)
+
     let offer = {
         Asset : "High",
         Type : "Market",
         Action : "Sell",
-        Quantity : parseInt(document.getElementById("HighSellMarket").value)
+        Quantity : quantity
     }
-    liveSend(offer)
+
+    if ((quantity >= high_risk_quantity)){
+        alert(`
+            \t NO PROCEDE LA OFERTA DE VENTA \t \n
+            Nota: \t
+            • Ofreces vender una cantidad: ${quantity} 
+            • la cual supera la cantidad que tienes de este activo: ${low_risk_quantity} 
+        `)
+    }else{
+        liveSend(offer)
+    }
 }
 
 function HighBuy() {
+
+    let holdings = parseFloat(document.getElementById("Total").value);
+
+    let quantity = parseInt(document.getElementById("HighBuyMarket").value)
+
     let offer = {
         Asset : "High",
         Type : "Market",
         Action : "Buy",
-        Quantity : parseInt(document.getElementById("HighBuyMarket").value)
+        Quantity : quantity
     }
-    liveSend(offer)
+
+    let amount = 0 * quantity;
+
+    if (amount > holdings){
+        alert(`
+            \t NO PROCEDE LA OFERTA DE COMPRA \t \n
+            Nota: \t
+            • Ofreces pagar un precio: ${price} por una cantidad: ${quantity} 
+            • lo cual supera tu actual presupuesto de: ${holdings} 
+        `)
+    }else{
+        liveSend(offer)
+    }
 }
 
 function LowSell() {
+
+    let low_risk_quantity = parseInt(document.getElementById("Low").value);
+
+    let quantity = parseInt(document.getElementById("LowSellMarket").value)
+
     let offer = {
         Asset : "Low",
         Type : "Market",
         Action : "Sell",
-        Quantity : parseInt(document.getElementById("LowSellMarket").value)
+        Quantity : quantity
     }
-    liveSend(offer)
+
+    if ((quantity >= low_risk_quantity)){
+        alert(`
+            \t NO PROCEDE LA OFERTA DE VENTA \t \n
+            Nota: \t
+            • Ofreces vender una cantidad: ${quantity} 
+            • la cual supera la cantidad que tienes de este activo: ${low_risk_quantity} 
+        `)
+    }else{
+        liveSend(offer)
+    }
 }
 
 function LowBuy() {
+
+    let holdings = parseFloat(document.getElementById("Total").value);
+
+    let quantity = parseInt(document.getElementById("LowBuyMarket").value)
+
     let offer = {
         Asset : "Low",
         Type : "Market",
         Action : "Buy",
-        Quantity : parseInt(document.getElementById("LowBuyMarket").value)
+        Quantity : quantity
     }
-    liveSend(offer)
+
+    let amount = 0 * quantity;
+
+    if (amount > holdings){
+        alert(`
+            \t NO PROCEDE LA OFERTA DE COMPRA \t \n
+            Nota: \t
+            • Ofreces pagar un precio: ${price} por una cantidad: ${quantity} 
+            • lo cual supera tu actual presupuesto de: ${holdings} 
+        `)
+    }else{
+        liveSend(offer)
+    }
 }
 
 function liveRecv(data) {
@@ -105,6 +237,11 @@ function liveRecv(data) {
     document.getElementById("LowRiskHoldings").innerHTML = '<tr><td>Low Risk</td><td>' + quantity.low_risk + '</td></tr>';
     document.getElementById("TotalHoldings").innerHTML = 'Capital =  '  + '<strong>' + holdings.total + ' Puntos</strong>'
 
+    document.getElementById("Total").value = holdings.total;
+    document.getElementById("Low").value = quantity.low_risk;
+    document.getElementById("High").value = quantity.high_risk;
+
+
     document.getElementById("HighBuyTable").innerHTML = ""
     document.getElementById("HighSellTable").innerHTML = ""
     document.getElementById("LowBuyTable").innerHTML = ""
@@ -112,7 +249,7 @@ function liveRecv(data) {
 
     for (let order of high_risk_orders) { 
 
-        let color_order = order.player_id == player_id ? "bbbbfc" : "ababad";
+        let color_order = order.player_id == player_id ? "#bbbbfc" : "#ababad";
 
         let buy_orders_table = document.getElementById("HighBuyTable");
         let sell_orders_table = document.getElementById("HighSellTable");
@@ -126,7 +263,7 @@ function liveRecv(data) {
 
     for (order of low_risk_orders) { 
 
-        let color_order = order.player_id == player_id ? "bbbbfc" : "ababad";
+        let color_order = order.player_id == player_id ? "#bbbbfc" : "#ababad";
 
         let buy_orders_table = document.getElementById("LowBuyTable");
         let sell_orders_table = document.getElementById("LowSellTable");
